7d6695137cd2f54f0b4947bbc9e88faa5693cecd
Vladislav <vlad.tiw27@gmail.com>
2020-12-22 00:08:15 +0200

    add script for burger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+//menu 
+const burger = document.querySelector('.header__burger'),
+      menu = document.querySelector('.header__nav');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('header__burger_active');
+    menu.classList.toggle('header__nav_active');
+});
+
+
 
 //gallery
 lightGallery(document.getElementById('lightgallery'));
@@ -9,4 +20,6 @@ const mySwiper = new Swiper('.swiper-container', {
     hashNavigation: {
         watchState: true
     }
+});
+
 });
